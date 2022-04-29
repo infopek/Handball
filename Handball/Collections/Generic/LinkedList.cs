5488e05afe0bc9dcc616d233a448c4abadea28e5
infopek <10saero01@gmail.com>
2022-04-29 23:35:33 +0200

    Add getter for head in LinkedList

--- Handball/Collections/Generic/LinkedList.cs
+++ Handball/Collections/Generic/LinkedList.cs
@@ -7,23 +7,24 @@ namespace Handball.Collections.Generic
     public delegate void TraverseHandler<T>(T data);
     public class LinkedList<T> : IEnumerable<T> where T : IComparable<T>
     {
-        private Node<T> head;
+        private Node<T> _head;
 
         public LinkedList()
         {
-            this.head = null;
+            this._head = null;
         }
 
+        public T Get_Head() => _head.Data;
         public void Insert(T data)
         {
             Node<T> newNode = new Node<T>();
             newNode.Data = data;
 
-            Node<T> curr = head;
+            Node<T> curr = _head;
             Node<T> prev = null;
 
             // Iterate through the list until a "greater" element is found
-            curr = head;
+            curr = _head;
             while (curr != null && curr.Data.CompareTo(data) < 0)
             {
                 prev = curr;
@@ -32,8 +33,8 @@ namespace Handball.Collections.Generic
             if (prev == null)
             {
                 // The list is empty
-                newNode.Next = head;
-                head = newNode;
+                newNode.Next = _head;
+                _head = newNode;
             }
             else
             {
@@ -43,7 +44,7 @@ namespace Handball.Collections.Generic
         }
         public T Search(Predicate<T> conditions)
         {
-            Node<T> curr = head;
+            Node<T> curr = _head;
             while (curr != null)
             {
                 if (conditions(curr.Data))               
@@ -55,7 +56,7 @@ namespace Handball.Collections.Generic
         }
         public void Delete(T item)
         {
-            Node<T> curr = head;
+            Node<T> curr = _head;
             Node<T> prev = null;
 
             // Deletes the first element that satisfies the conditions
@@ -69,8 +70,8 @@ namespace Handball.Collections.Generic
                 // The list contains such element
                 if (prev == null)
                 {
-                    // The element to delete is the head
-                    head = curr.Next;
+                    // The element to delete is the _head
+                    _head = curr.Next;
                 }
                 else
                 {
@@ -85,7 +86,7 @@ namespace Handball.Collections.Generic
         public void Traverse(TraverseHandler<T> handler)
         {
             TraverseHandler<T> _handler = handler;
-            Node<T> curr = head;
+            Node<T> curr = _head;
             while (curr != null)
             {
                 _handler?.Invoke(curr.Data);
@@ -93,7 +94,7 @@ namespace Handball.Collections.Generic
             }
         }
 
-        public IEnumerator<T> GetEnumerator() => new Traverser<T>(head);
-        IEnumerator IEnumerable.GetEnumerator() => new Traverser<T>(head);
+        public IEnumerator<T> GetEnumerator() => new Traverser<T>(_head);
+        IEnumerator IEnumerable.GetEnumerator() => new Traverser<T>(_head);
     }
 }
